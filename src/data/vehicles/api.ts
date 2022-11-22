2203e54fcafffe87797982224c673c2d532735db
Aleksey Mikhailenko <maks.quantum@yandex.ru>
2022-11-22 12:43:01 +0300

    change func

--- src/data/vehicles/api.ts
+++ src/data/vehicles/api.ts
@@ -5,6 +5,6 @@ const vehicle = generateVehicle(15);
 
 export class VehicleApi {
     static search({ type, title }: VehicleFilter): Vehicle[] {
-        return vehicle.filter(x => (type == null || x.type === type) && x.title.toLowerCase().search(title.toLowerCase()) !== -1);
+        return vehicle.filter(x => (type == null || x.type === type) && x.title.search(title) !== -1);
     }
 }
